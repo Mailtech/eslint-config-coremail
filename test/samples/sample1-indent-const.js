70cc943ab8f7c4ae011149b9897910796a62c036
William Leung <williamleung2006@gmail.com>
2024-10-16 11:31:09 +0800

    release: 1.0 for eslint 9 - migrate to stylistic rules
    - breaking: `@stylistic/eslint-plugin` rules migration
      following this guide https://eslint.style/guide/migration
    - remove dependency `eslint-config-standard`, all rules imported (with deprecated rules replacing)
    - bump `eslint-plugin-import` and restore rule 'import/export'
      ref https://github.com/import-js/eslint-plugin-import/issues/2556
    
    new / changed rules
    - disable 'import/no-webpack-loader-syntax' (imported webmail rules)
    - add 'no-restricted-globals' to forbid using global event (imported webmail rules)
    - change 'generator-star-spacing' and 'yield-star-spacing' to always 'after'

--- test/samples/sample1-indent-const.js
+++ test/samples/sample1-indent-const.js
@@ -1,9 +1,8 @@
 //
-// The indent rule do not supports '1.5' as unit (6 space) or other multiline alignment technic
-/* eslint indent: "off" */
+// The indent rule does not support '1.5' as unit (6 space) or other multiline alignment technic
+/* eslint @stylistic/indent: "off" */
 
 // noinspection JSUnusedGlobalSymbols
 const A = 'a',
       B = 'b',
       C = 'c';
-
